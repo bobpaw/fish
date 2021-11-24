3995e2306c065873a6587e9fcbb344ce338ee448
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-11-24 11:08:30 -0600

    Only change is adding &
    Leaves stack like gmnyx where g is gcd(m,n)

--- eea.fish
+++ eea.fish
@@ -1,7 +1,6 @@
-l2(?;:{:})?r>   :?\&:}1>l4)?!;4[&{:{:@{{-{,&]:@*&+$&\
-            ]     2
-            %     [
-            \$}:{:/    \                            /
-
+l2(?;:{:})?r>   :?\&:}1>l  4  )?\&;
+            ]     2    \&$+&*@\ 4
+            %     [    /-{,&]:/ [
+            \$}:{:/    \{{@:{:{&/
 
 Eventually an Extended Euclidean Algorithm Evaluator. Currently a work in progress.
