32698698033c3eb7c2f307c164c37f7db0dc8635
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-11-24 10:51:46 -0600

    Calculate x,y values
    Currently calculates and ends with y on top of the stack and x in the register.

--- eea.fish
+++ eea.fish
@@ -1,4 +1,7 @@
-l2(?;:{:})?r>:?!;2[:{:}$%]\
-            \             /
+l2(?;:{:})?r>   :?\&:}1>l4)?!;4[&{:{:@{{-{,&]:@*&+$&\
+            ]     2
+            %     [
+            \$}:{:/    \                            /
+
 
 Eventually an Extended Euclidean Algorithm Evaluator. Currently a work in progress.
